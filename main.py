4fef8ab268c7e90fcb80de4658f70eacabd8c899
Siddharth Singh <tosiddharthsingh@gmail.com>
2025-01-23 14:51:16 -0600

    testing if thisll reach github
    Please

---
+++ main.py
@@ -0,0 +1,10 @@
+# this is a very barebones version and ill add more in the future
+
+password = input("Enter password: ")
+
+if(len(password) < 8):
+    print("Bad password. Booooo")
+else:
+    print("Good password. Nice")
+
+#what to add next
